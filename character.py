998bf518b0ed0f1525463f6efa2f95d276774fc0
Clodagh Campbell <campbellc@macstudent.com.au>
2025-06-17 16:35:38 +1000

    progress in character methods

--- character.py
+++ character.py
@@ -2,18 +2,17 @@
 # - - - - - - - CHARACTER SUPERCLASS - - - - - - - #
 
 class Character():
-    def __init__(self, char_name, char_description, char_health, char_attack):
+    def __init__(self, char_name, char_description, char_health):
         self.name = char_name
         self.description = char_description
         self.health = char_health
-        self.attack = char_attack
 
 # - - - - - - - NOTES - - - - - - - #
 
 #   Common features in Character
 # 1. A name
 # 2. A note entry including their description
-# 3. Health and battle stats
+# 3. Health stats
 
 # - - - - - - - METHODS CHARACTER - - - - - - - #
 
@@ -38,37 +37,14 @@ class Character():
 
     def get_health(self):
         return self.health
-    
-    def set_attack(self, char_attack):
-        self.attack = char_attack
-
-    def get_attack(self):
-        return self.attack
-
-# 4. DIALOGUE
-
-    def talk(self):
-        if self.conversation is not None:
-            print("[" + self.name + "]: " + self.conversation)
-        else:
-            print(self.name + "really isn't in the mood.")
-
-# (Neutral)
-    def set_convo_intro(self, intro_convo):
-        self.introconvo = intro_convo
-# (Passive)
-    def set_convo_pas(self, pas_convo):
-        self.pasconvo = pas_convo
-# (Agitated)
-    def set_convo_ag(self, ag_convo):
-        self.agconvo = ag_convo
 
 # - - - - - - - ENEMY SUBCLASS - - - - - - - #
 
 class Enemy(Character):
-    def __init__(self, char_name, char_description, char_health, char_attack):
-        super().__init__(char_name, char_description, char_health, char_attack)
-        self.weakness = None
+    def __init__(self, char_name, char_description, char_health, en_weak):
+        super().__init__(self, char_name, char_description, char_health)
+        self.weakness = en_weak
+        self.exhaust = 0
 
 # - - - - - - - NOTES - - - - - - - #
 
@@ -88,10 +64,16 @@ class Enemy(Character):
 # FIGHT
 
 # EXHAUSTION
+    def exhaust(self):
+        return
 
 # STEAL
     def steal(self):
-        print("You steal from", + self.name)
+        if self.exhaust >= 80:
+            print("You stole PLACEHOLDER from " + self.name)
+        else:
+            print(self.name + "is on high alert")
+
 
 # - - - - - - - FRIEND SUBCLASS - - - - - - - #
 
@@ -102,6 +84,7 @@ class Friend(Character):
 
 # - - - - - - - NOTES - - - - - - - #
 
+#   Friend inherits from character and adds:
 # - Introductory dialogue
 # - Varied passive dialogue
 
@@ -115,17 +98,13 @@ class Friend(Character):
 class Merchant(Friend):
     def __init__(self, char_name, char_description):
         super().__init__(char_name, char_description)
-        self.feeling = None
 
 # - - - - - - - NOTES - - - - - - - #
 
-#   Merchants inherit from friends and adds:
+#   Merchants inherit from Friend and adds:
 # - The ability to return currency when item is given
 
-# - - - - - - - FRIEND METHODS - - - - - - - #
-
-    def pat(self):
-        print(self.name + "pats you")
+# - - - - - - - MERCHANT METHODS - - - - - - - #
 
 # - - - - - - - x - - - - - - - #
 
@@ -134,15 +113,39 @@ class Merchant(Friend):
 
 # Idk what this is for
 
-    def set_combat_item(self, combat_item):
-        self.combat_item = combat_item
+#     def set_combat_item(self, combat_item):
+#         self.combat_item = combat_item
 
-    def fight(self, combat_item):
-        if combat_item == self.weakness:
-            print("[" + self.name + "]: " + "What... is that.")
-            print("You've discovered the enemy's weakness")
-            print("Attacker's defense has dropped by 50%")
+#     def fight(self, combat_item):
+#         if combat_item == self.weakness:
+#             print("[" + self.name + "]: " + "What... is that.")
+#             print("You've discovered the enemy's weakness")
+#             print("Attacker's defense has dropped by 50%")
 
-    def fight(self):
-        print(self.name + "isn't bothered enough.")
-        return True
+#     def fight(self):
+#         print(self.name + "isn't bothered enough.")
+#         return True
+    
+#     def set_attack(self, char_attack):
+#         self.attack = char_attack
+
+#     def get_attack(self):
+#         return self.attack
+    
+# # 4. DIALOGUE
+
+#     def talk(self):
+#         if self.conversation is not None:
+#             print("[" + self.name + "]: " + self.conversation)
+#         else:
+#             print(self.name + "really isn't in the mood.")
+
+# # (Neutral)
+#     def set_convo_intro(self, intro_convo):
+#         self.introconvo = intro_convo
+# # (Passive)
+#     def set_convo_pas(self, pas_convo):
+#         self.pasconvo = pas_convo
+# # (Agitated)
+#     def set_convo_ag(self, ag_convo):
+#         self.agconvo = ag_convo
