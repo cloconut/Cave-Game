f56f1e2b6fa8079ed514f385403574af65d67daf
cloconut <campbellc@macstudent.com.au>
2025-06-17 01:32:55 +0000

    modifying cave attributes and methods

--- cave.py
+++ cave.py
@@ -1,64 +1,75 @@
+# - - - - - - - NOTES - - - - - - - #
+
+#   Caves will have the following features:
+# - An enterance sign displaying its name
+# - Caves linked to it / map features
+# - Notes from the previous explorer that player finds on entry
+# - One enemy minimum
+# - One friend minimum (includess merchants)
+# - 2 items minimum (defense and attack)
+
 # - - - - - - - CAVE SUPERCLASS - - - - - - - #
 
 class Cave:
     def __init__(self, cave_name):
         self.name = cave_name
-        self.description = None
+        self.note = None
         self.linked_caves = {}
         self.character = None
         self.item = None
 
 # - - - - - - - METHODS (CAVE) - - - - - - - #
 
-    # Set the cave description
-    def set_description(self, cave_description):
-        self.description = cave_description
+# 1. NAME
 
-    # Get the cave description
-    def get_description(self):
-        return self.description
-
-    # Set the cave name
     def set_name(self, cave_name):
         self.name = cave_name
 
-    # Get the cave name
     def get_name(self):
         return self.name
 
-    # Character getting and setting ???????????
-    def set_character(self, new_character):
-        self.character = new_character
+# 2. LINKED CAVES / MAP FEATURES
 
-    def get_character(self):
-        return self.character
-
-    def set_item(self, new_item):
-        self.item = new_item
-
-    def get_item(self):
-        return self.item
-
-    # Display the cave description
-    def describe(self):
-        print(self.description)
-
-    # Link cave method
     def link_cave(self, cave_to_link, direction):
         self.linked_caves[direction] = cave_to_link
 
-    # Cave details
-    def get_details(self):
+    def get_map(self):
         for direction in self.linked_caves:
             cave = self.linked_caves[direction]
             print('The ' + cave.get_name() + ' is ' + direction)
 
-    # Move around the cave
     def move(self, direction):
         if direction in self.linked_caves[direction]:
             return self.linked_caves[direction]
         else:
             print("You can't go that way")
             return self
+
+# 3. EXPLORER NOTES
+
+    def set_note(self, cave_note):
+        self.note = cave_note
+
+    def get_note(self):
+        return self.note
+
+    def note(self):
+        print(self.note)
+
+# 4. CHARACTERS
+
+    def set_character(self, new_character):
+        self.character = new_character
+
+    def get_character(self):
+        return self.character
+
+# 5. ITEMS
+
+    def set_item(self, new_item):
+        self.item = new_item
+
+    def get_item(self):
+        return self.item
 
 # - - - - - - - x - - - - - - - #
